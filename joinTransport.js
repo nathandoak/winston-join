7d77c42850d59a8e50f9b7e718c3185d9e542c50
Nathan Doak <nathan.doak@gmail.com>
2018-01-17 22:31:37 -0800

    Building out base functionality

--- joinTransport.js
+++ joinTransport.js
@@ -1,20 +1,33 @@
 var winston = require('winston');
 var fetch = require('node-fetch');
+var querystring = require('querystring');
 var util = require('util');
 
-function JoinTransport (ApiKey, Options) {
-    var apiKey = ApiKey;
-    var options = Options;
+function JoinTransport (ApiKey, Devices, Options) {
+    if (!ApiKey) {
+        throw "ApiKey is required";
+    }
 
+    if (!Devices) {
+        throw "Device id(s) or group name is required";
+    }
 
+    this.apiData = Object.assign({ apikey: ApiKey, deviceId: Devices}, Options);
 }
 
 util.inherits(JoinTransport, winston.Transport);
 
 JoinTransport.prototype.log = function (level, message, meta, callback) {
-    console.log(message);
-    callback(null, true);
+    this.apiData.text = message;
+    this.apiData.title = level;
+
+    fetch(`${this.apiUrl}?${querystring.stringify(this.apiData)}`)
+        .then((response) => {
+            //console.log(response.body);
+        })
+        .then(() => callback(null, true));
 }
 
+JoinTransport.prototype.apiUrl = 'https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush';
 
 module.exports = JoinTransport;
